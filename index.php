802a1d772fb2dfea2054a4fe1190e09a86268e9a
jasmindominique <jsurpris.jd@gmail.com>
2019-09-30 20:16:10 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php 
+
+	$site_title = "My First PHP Image Gallery";
+
+	$gallery = [
+		[
+			'src' => 'https://picsum.photos/seed/1/800/600',
+			'name' => 'Stuff'
+		],
+		[
+			'src' => 'https://picsum.photos/seed/2/800/600',
+			'name' => 'Things'
+		],
+		[
+			'src' => 'https://picsum.photos/seed/3/800/600',
+			'name' => 'Picture'
+		],
+		[
+			'src' => 'https://picsum.photos/seed/4/800/600',
+			'name' => 'Whatever'
+		],
+		[
+			'src' => 'https://picsum.photos/seed/5/800/600',
+			'name' => 'Image'
+		],
+		[
+			'src' => 'https://picsum.photos/seed/6/800/600',
+			'name' => 'What'
+		],
+		[
+			'src' => 'https://picsum.photos/seed/7/800/600',
+			'name' => 'Wat'
+		],
+		[
+			'src' => 'https://picsum.photos/seed/8/800/600',
+			'name' => 'Foo'
+		],
+		[
+			'src' => 'https://picsum.photos/seed/9/800/600',
+			'name' => 'Baz'
+		],
+		[
+			'src' => 'https://picsum.photos/seed/10/800/600',
+			'name' => 'Thingies'
+		],
+		[
+			'src' => 'https://picsum.photos/seed/11/800/600',
+			'name' => 'Boom'
+		],
+		[
+			'src' => 'https://picsum.photos/seed/12/800/600',
+			'name' => 'Out of ideas'
+		],
+		[
+			'src' => 'https://picsum.photos/seed/13/800/600',
+			'name' => '???'
+		],
+	];
+?>
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<title> </title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel="stylesheet" href="dist/css/main.css">
+
+	</head>
+	<body>
+
+		<h1> </h1>
+
+		<h3>Showing  images.</h3>
+
+		<ul class="gallery">
+			
+		</ul>
+
+		<script src="dist/js/main.js"></script>
+
+	</body>
+</html>
