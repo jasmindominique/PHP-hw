1fa05c5b8627e02b89015768e68a7231de87f487
jasmindominique <jsurpris.jd@gmail.com>
2019-10-07 12:09:01 -0400

    php changes - test

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
 			'name' => 'Boom'
 		],
 		[
-			'src' => 'https://picsum.photos/seed/12/800/600',
+			'src' => 'https://picsum.photos/seed/14/800/600',
 			'name' => 'Out of ideas'
 		],
 		[
@@ -67,16 +67,81 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<link rel="stylesheet" href="dist/css/main.css">
+		<link href="https://fonts.googleapis.com/css?family=Poppins:100,400,700&display=swap" rel="stylesheet">
 
 	</head>
 	<body>
 
-		<h1> </h1>
+		<!-- <style type="text/css">
+			body{
+				
+			}
+		</style> -->
 
-		<h3>Showing  images.</h3>
+		<h1><?php echo $site_title;?>
+			<style type="text/css">
+				h1{ color: #6577C0;
+					font-family: 'Poppins', sans-serif;
+					font-weight: 700;
+					text-align: center;
+					margin-top: 5%; 
+				}
+			</style>
+		</h1>
+
+		<br>
+
+		<h3>Showing images <?php echo count ($gallery)?> of
+			<?php echo count ($gallery) ?>
+			<style type="text/css">
+				h3{
+					font-family: 'Poppins', sans-serif;
+					font-weight: 300;
+					text-align: center;
+					margin:20px auto;
+				}
+			</style>
+		</h3>
+
+		<br>
 
 		<ul class="gallery">
-			
+			<?php 
+				foreach ($gallery as $i => $photo) { 
+			?> 
+
+			<!-- <style type="text/css">
+				.gallery{
+					margin-left: 20px;
+					margin-right: 20px;
+				}
+			</style> -->
+
+			<li>
+				<style type="text/css">
+				li{
+					padding: 20px;
+					font-family: 'Poppins', sans-serif;
+					font-style: thin;
+				}
+				</style>
+				
+				<?php
+					echo $i, ".";
+					echo "<img src='",$photo['src']," '> ";
+					echo "<h4>", $photo['name'],"</h4>";
+					}
+				?>  
+				<style type="text/css">
+				h4{
+					color: #6577C0;
+					font-size: 20px;
+					font-weight: 400;
+					text-align: center;
+					margin-top: 5px;
+				}
+				</style>
+			</li> 
 		</ul>
 
 		<script src="dist/js/main.js"></script>
